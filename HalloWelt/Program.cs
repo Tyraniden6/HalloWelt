9390fd8dbb5bad64dd206ce6190c69e54b069fde
Tyraniden6 <cadets11@unitybox.de>
2018-01-10 10:20:59 +0100

    Update Program.cs
    New Line

--- HalloWelt/Program.cs
+++ HalloWelt/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace HalloWelt
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Über Github");
             Console.ReadLine();
         }
     }
